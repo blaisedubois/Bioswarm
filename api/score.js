1e712fe4ec708209dce01196b8456c6daa7e2212
blaisedubois <blaiseduboisart@gmail.com>
2025-06-30 14:12:42 +0200

    Update score.js

--- api/score.js
+++ api/score.js
@@ -3,10 +3,11 @@ export default async function handler(req, res) {
     return res.status(405).json({ status: 'error', message: 'Method not allowed' });
   }
 
-const { telegram_id, username, profile_photo, score } = req.body;
+  const { telegram_id, username, profile_photo, score } = req.body;
 
-if (!telegram_id || !username || !profile_photo || !score) {
-  return res.status(400).json({ status: 'error', message: 'Missing fields' });
+  if (!telegram_id || !username || !profile_photo || !score) {
+    return res.status(400).json({ status: 'error', message: 'Missing fields' });
+  }
+
+  return res.status(200).json({ status: 'success', message: 'Received fields' });
 }
-
-return res.status(200).json({ status: 'success', message: 'Received fields' });
