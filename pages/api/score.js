95742e27cf811115745b98d744e133e3c1238ae2
blaisedubois <blaiseduboisart@gmail.com>
2025-07-04 10:50:36 +0200

    cors debugging

--- pages/api/score.js
+++ pages/api/score.js
@@ -1,12 +1,9 @@
 import { supabase } from '../../supabaseClient';
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Vary', 'Origin');
-
-
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', '*');
+res.setHeader('Access-Control-Allow-Headers', '*');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
