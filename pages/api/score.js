fc0df040ce3f6f24343bbde0c52bd97a8a58e227
blaisedubois <blaiseduboisart@gmail.com>
2025-07-04 10:23:34 +0200

    cors change

--- pages/api/score.js
+++ pages/api/score.js
@@ -1,7 +1,8 @@
 import { supabase } from '../../supabaseClient';
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
